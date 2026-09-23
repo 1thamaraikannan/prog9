b221b5e9dff5cf8bf5364974ddb8920fb8b36c20
1thamaraikannan <1thamaraikannan63@gmail.com>
2026-09-23 16:31:32 +0530

    Update test_firewall.sh

--- tests/test_firewall.sh
+++ tests/test_firewall.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-set -u
+set -u#!/bin/bash
+
+# Test 1: Open TCP port 8080
+firewall-cmd --add-port=8080/tcp
+
+# Test 2: Open TCP port 9000
+firewall-cmd --add-port=9000/tcp
+
+# Test 3: List configured ports
+firewall-cmd --list-ports
+
+# Test 4: Remove TCP port 8080
+firewall-cmd --remove-port=8080/tcp
+
+# Test 5: Permanently open TCP port 3000
+firewall-cmd --add-port=3000/tcp --permanent
+
+# Test 6: Reload firewall
+firewall-cmd --reload
 
 SCRIPT="$1"
 
